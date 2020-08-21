df1f9fde548c8375c741778fe2005f76ee835136
Gabriel Gimenes <gabrielgrock@gmail.com>
2020-08-21 14:48:19 -0300

    ConsoleApp

---
+++ GaugeTestsGenerator.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GaugeTestsGenerator.ConsoleApp
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
