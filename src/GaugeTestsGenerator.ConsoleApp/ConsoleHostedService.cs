867dd8cc2dc065c07c31ddaa7679ef0036c8975c
Gabriel Gimenes <gabrielgrock@gmail.com>
2020-08-21 18:48:07 -0300

    Configuring DI

---
+++ src/GaugeTestsGenerator.ConsoleApp/ConsoleHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GaugeTestsGenerator.ConsoleApp
+{
+    public class ConsoleHostedService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/GaugeTestsGenerator.ConsoleApp/Program.cs
+++ src/GaugeTestsGenerator.ConsoleApp/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace GaugeTestsGenerator.ConsoleApp
@@ -7,7 +12,24 @@ namespace GaugeTestsGenerator.ConsoleApp
     {
         public async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", true);
+                    if (args != null) config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostingContext, services) =>
+                {
+                    services.AddHostedService<ConsoleHostedService>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration);
+                    logging.AddConsole();
+                });
+
+            await builder.RunConsoleAsync();
         }
     }
 }
